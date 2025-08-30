5ee49172e800720f681adadcbb0fab65435aa1c8
H82M19\Student <Student>
2025-08-30 14:33:14 +0700

    30. Use async/await + Promise.allSettled() to handle multiple API calls and display their success/failure status.

---
+++ Tuan02/dist/Bai30.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fakeApiCall = (url, shouldFail = false) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (shouldFail) {
+                reject('Failed');
+            }
+            else {
+                resolve('Success');
+            }
+        }, 1000);
+    });
+};
+const handleMultipleApiCalls = async () => {
+    const apiCalls = [
+        fakeApiCall("https://api.example.com/user"),
+        fakeApiCall("https://api.example.com/posts", true),
+        fakeApiCall("https://api.example.com/comments"),
+        fakeApiCall("https://api.example.com/likes", true),
+        fakeApiCall("https://api.example.com/friends")
+    ];
+    const results = await Promise.allSettled(apiCalls);
+    results.forEach((result, index) => {
+        if (result.status === "fulfilled") {
+            console.log(`Task ${index + 1}: ${result.value}`);
+        }
+        else {
+            console.log(`Task ${index + 1}: ${result.reason}`);
+        }
+    });
+};
+handleMultipleApiCalls();
